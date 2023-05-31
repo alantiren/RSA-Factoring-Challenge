1baab461c824316411f3ee62f487c877dc541030
alantiren <tirenalan0@gmail.com>
2023-05-31 21:48:58 +0300

    updates

--- factors.c
+++ factors.c
@@ -1,48 +1,46 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
-std::vector<int> factorize(int number) {
-    std::vector<int> factors;
-    for (int i = 2; i * i <= number; i++) {
-        while (number % i == 0) {
-            factors.push_back(i);
-            number /= i;
-        }
-    }
-    if (number > 1) {
-        factors.push_back(number);
-    }
-    return factors;
+void factorize(int number)
+{
+int i, p, q;
+for (i = 2; i * i <= number; i++)
+{
+while (number % i == 0)
+{
+p = i;
+q = number / i;
+printf("%d=%d*%d\n", number, p, q);
+number = q;
+}
+}
+if (number > 1)
+{
+p = number;
+q = 1;
+printf("%d=%d*%d\n", number, p, q);
+}
 }
 
-void printFactorization(int number, int p, int q) {
-    std::cout << number << "=" << p << "*" << q << "\n";
+int main(int argc, char* argv[])
+{
+if (argc != 2) {
+printf("Usage: factors <file>\n");
+return (0);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cout << "Usage: factors <file>\n";
-        return 0;
-    }
+FILE* file = fopen(argv[1], "r");
+if (!file)
+{
+printf("Failed to open the file.\n");
+return (0);
+}
 
-    std::ifstream file(argv[1]);
-    if (!file.is_open()) {
-        std::cout << "Failed to open the file.\n";
-        return 0;
-    }
+int number;
+while (fscanf(file, "%d", &number) != EOF) {
+factorize(number);
+}
 
-    int number;
-    while (file >> number) {
-        std::vector<int> factors = factorize(number);
-        int size = factors.size();
-        for (int i = 0; i < size / 2; i++) {
-            int p = factors[i];
-            int q = factors[size - i - 1];
-            printFactorization(number, p, q);
-        }
-    }
-
-    file.close();
-    return 0;
+fclose(file);
+return (0);
 }
